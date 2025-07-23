08d5b2b821dbf8f22bfa3fcc90c7746cca871213
Mauro Castro <49006241+MauroCastro1705@users.noreply.github.com>
2025-07-22 22:45:48 -0300

    enemy ship assets

--- enemies/enemigo_1.gd
+++ enemies/enemigo_1.gd
@@ -6,8 +6,10 @@ var hp
 var active = false
 var is_dead = false
 @onready var damage_numbers_origin: Node2D = $damage_numbers_origin
-@onready var sprite = %ship
+@onready var ship_sprite = %ship
 @onready var health_bar: Control = %HealthBar
+@onready var ship: Node2D = $Node2D
+@onready var propulsor: AnimatedSprite2D = %propulsor
 
 #----------------------------
 #COMO MOSTRAR NUMEROS DE DAÑO
@@ -29,9 +31,10 @@ func _process(_delta):
 		return
 	if Global.player and is_instance_valid(Global.player):
 		var dir = (Global.player.global_position - global_position).normalized()
-		look_at(Global.player.global_position )
 		velocity = dir * speed
+		ship.rotation = dir.angle() + deg_to_rad(90)
 		move_and_slide()
+		ship_sprite.show()
 		health_bar.rotation = 0
 
 
@@ -43,35 +46,47 @@ func activate(pos: Vector2):
 	show()
 	set_process(true)
 	health_bar.set_max_health(hp)
+	ship_sprite.play("alive")
+	propulsor.play("alive")
 
 
-func deactivate():
-	hp = Global.enemigo_max_hp
-	health_bar.set_max_health(hp)
-	active = false
-	is_dead = true
-	hide()
-	set_process(false)
-	
-	# Inform player we're out of range (if needed)
-	if Global.player:
-		Global.player.remove_enemy(self)
 
 func take_damage(amount):
 	if not active or is_dead:
 		return
 	hp -= amount
 	health_bar.update_health(hp)
+	ship_sprite.visible = false
 
 	DamageNumbers.display_numbers(amount, damage_numbers_origin.global_position)
-	DamageNumbers.flash_sprite(sprite)
+	DamageNumbers.flash_sprite(ship_sprite)
 	if hp <= 0:
 		is_dead = true
 		Global.emit_signal("enemy_killed")
-		deactivate()
+		propulsor.visible = false
+		ship_sprite.play("die")
+
 		
-		
+func deactivate():
+	hp = Global.enemigo_max_hp
+	health_bar.set_max_health(hp)
+	active = false
+	is_dead = true
+	set_process(false)
+	hide()
+	# Inform player we're out of range (if needed)
+	if Global.player:
+		Global.player.remove_enemy(self)
+
+
 func enemy_update_stats():
 	speed = Global.enemigo_speed
 	max_hp = Global.enemigo_max_hp
 	
+
+
+
+
+func _on_ship_animation_finished() -> void:
+	if ship_sprite.animation == "die" and is_dead:
+		deactivate()
